a5267190b0a904f00bcd2cf613a6943491474006
Lone Wolf History <31628833+levigoddard@users.noreply.github.com>
2020-04-09 11:30:13 -0600

    !1.0.0.1

---
+++ Culture Influince/common/defines/ci_defines.lua
@@ -0,0 +1,18 @@
+NDefines.NAI.AI_CONVERT_CULTURES = 0 					--AI uses this very stupidly
+NDefines.NCountry.CORE_LOSE_CULTURE_GROUP = -1 			--This leads to better gameplay with CI
+NDefines.NCountry.MONTHS_TO_CHANGE_CULTURE = 20 		--4x Increase
+NDefines.NCountry.PS_CHANGE_CULTURE = 20				--4x Increase
+NDefines.NCountry.ENFORCE_CULTURE_TAX_MULTIPLIER = 0.5	--2x Decrease 
+NDefines.NAI.CHARTER_COMPANY_BASE_RELUCTANCE = -10000	--Disabled
+
+	--Steppe Improvements (To be splintered into separate mod later)
+NDefines.NCountry.RAZE_UNREST_DURATION = 10							--Same
+NDefines.NCountry.RAZE_PROVINCE_DEVELOPMENT_DECREASE = 0.33			--Same
+NDefines.NCountry.RAZE_PROVINCE_POWER_PER_DEVELOPMENT = 10			--From 25p0
+NDefines.NCountry.RAZE_TECH_BASE_LEVEL = 12							--3 -> 12 (1557)
+NDefines.NCountry.RAZE_TECH_POWER_DECREASE = 0.02					--0p04 -> 0p02
+NDefines.NCountry.RAZE_TECH_POWER_DECREASE_MAX = 0.4				--0p02 -> 0p04 (Want to be harsher but have a reduced gradient)
+NDefines.NCountry.RAZE_PROVINCE_COOLDOWN_YRS = 20					--Same
+NDefines.NCountry.RAZE_PROVINCE_CONQUERED_TIME_LIMIT_MONTHS = 6		--Same
+NDefines.NCountry.HORDE_UNITY_PER_LOOT = 0.25						--Same
+NDefines.NCountry.HORDE_UNITY_PER_RAZE = 0.25						--0p5 -> 0p25
---
+++ Culture Influince/descriptor.mod
@@ -0,0 +1,13 @@
+version="0.5.14"
+tags={
+	"Events"
+	"Gameplay"
+}
+dependencies={
+	"Beyond Typus (hopefully temp post 1.29 launcher)"
+	"BT Addon: Bugfixing BT 12.0"
+}
+picture="Thumbnail.png"
+name="[BETA] Beyond Typus Addon : Cultural Influence | Version 0.5.14 Overhaul"
+supported_version="1.29.*.*"
+remote_file_id="1374507960"
